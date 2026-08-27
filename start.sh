35e5ab497c0e93eab8ea4d55d0864656f877f773
Noah Maclean <noahmac5000@hotmail.com>
2026-08-27 17:41:33 +0100

    fixed ending the start script with Ctrl+C

--- start.sh
+++ start.sh
@@ -67,7 +67,8 @@ echo "Vite frontend running with PID $FRONTEND_PID"
 
 echo "=================================================="
 echo "🚀 Collab DAW is fully running!"
-echo " - RONTEND_PID; docker compose down; exit" INT
+# echo " - RONTEND_PID; docker compose down; exit" INT
+trap "echo 'Shutting down'; docker compose down; kill $BACKEND_PID $FRONTEND_PID 2>/dev/null; exit" INT
 
 # Keep script running
 wait
